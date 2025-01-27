89f546772da592586bdefc0521f82082a8d6044e
Marat-terabyte <marat.amzph21@gmail.com>
2025-01-27 09:39:59 +0300

    Replaced

---
+++ Backend/Marketplace/Marketplace.Shared/Models/SearchIndexRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Marketplace.Shared.Models
+{
+    public class SearchIndexRequest
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public RequestType Type { get; set; }
+    }
+
+    public enum RequestType
+    {
+        Index, Deindex, Update
+    }
+}
